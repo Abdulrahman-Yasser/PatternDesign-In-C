f883f1fae76b3dbc93ff66dde0d14503d1e9c98c
Abdulrahman yasser <abdu.ya74@gmail.com>
2023-03-14 16:06:22 +0200

    just back from millitary service and i do not remember anything

---
+++ Chapter_2-Project/Mediator_Pattern/App/SpecificDrivers/LED_Driver/inc/LED.h
@@ -0,0 +1,15 @@
+/*
+ * LED.h
+ *
+ *  Created on: Mar 7, 2023
+ *      Author: Abdu
+ */
+
+#ifndef APP_SPECIFICDRIVERS_LED_DRIVER_INC_LED_H_
+#define APP_SPECIFICDRIVERS_LED_DRIVER_INC_LED_H_
+
+
+
+
+
+#endif /* APP_SPECIFICDRIVERS_LED_DRIVER_INC_LED_H_ */
---
+++ Chapter_2-Project/Mediator_Pattern/App/SpecificDrivers/LED_Driver/src/LED.c
@@ -0,0 +1,10 @@
+/*
+ * LED.c
+ *
+ *  Created on: Mar 7, 2023
+ *      Author: Abdu
+ */
+
+
+
+
